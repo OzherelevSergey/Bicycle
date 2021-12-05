94e9978086ae03fe57df91acef9ed5c2c13c3c15
SergeyOzherelev <sergeyozherelev86@gmail.com>
2021-12-05 23:47:52 +0200

    add block work to homeWork10

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //mobile menu
     $('.menu-bottom').on('click', function () {
         $('.navigation-wrapper').addClass('active');
     });
@@ -6,4 +7,24 @@ $(document).ready(function () {
     $('.close-menu').on('click', function () {
         $('.navigation-wrapper').removeClass('active');
     })
+
+    // slider swiper-reviews
+    const reviewsSlider = new Swiper('.swiper-reviews', {
+        loop: true,
+      
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+      
+      });
+
+
+
+
+
+
+
 });
+
+
