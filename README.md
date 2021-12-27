82c49f0ba789903d548aa818d994629faf195c75
SergeyOzherelev <sergeyozherelev86@gmail.com>
2021-12-27 19:35:42 +0200

    add blocks

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Bicycle
-First site
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
-    $('.menu-bottom').on('click', function () {
+    //Mobile menu
+    $('.menu-button').on('click', function() {
         $('.navigation-wrapper').addClass('active');
-    });
-
-    $('.close-menu').on('click', function () {
+    })
+    $('.close-menu').on('click', function() {
         $('.navigation-wrapper').removeClass('active');
     })
-});
+
+    //Reviews slider
+    const reviewsSlider = new Swiper('.swiper-reviews', {
+        loop: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+    });
+})
